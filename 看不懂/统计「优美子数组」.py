12007c8f70e57f84f9b8d48939f7883ee53a6106
litao <593516104@qq.com>
2020-04-21 20:30:12 +0800

    update

---
+++ 看不懂/统计「优美子数组」.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        odd = [-1]
+        ans = 0
+        for i in range(n):
+            if nums[i] % 2 == 1:
+                odd.append(i)
+        odd.append(n)
+        print(odd)
+        for i in range(1, len(odd) - k):
+            ans += (odd[i] - odd[i - 1]) * (odd[i + k] - odd[i + k - 1])
+        return ans
+
