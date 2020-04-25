2e449239d5315e679080fc1d48bea7d8eb926989
litao <593516104@qq.com>
2020-04-25 21:21:47 +0800

    update

---
+++ 多看几遍/全排列.py
@@ -0,0 +1,29 @@
+# 给定一个 没有重复 数字的序列，返回其所有可能的全排列。
+import itertools
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return list(itertools.permutations(nums))
+        def backtrack(first=0):
+            # 所有数都填完了
+            if first == n:
+                res.append(nums[:])
+            for i in range(first, n):
+                # 动态维护数组
+                nums[first], nums[i] = nums[i], nums[first]
+                # 继续递归填下一个数
+                backtrack(first + 1)
+                # 撤销操作
+                nums[first], nums[i] = nums[i], nums[first]
+
+        n = len(nums)
+        res = []
+        backtrack()
+        return res
+
+
+test = Solution()
+print(test.permute([1,2,3]))
---
+++ 完成/整数反转.py
@@ -0,0 +1,18 @@
+# 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x == 0:
+            return 0
+        str_x = str(x)
+        x = ''
+        if str_x[0] == '-':
+            x += '-'
+        x += str_x[len(str_x) - 1::-1].lstrip("0").rstrip("-")
+        x = int(x)
+        if -2 ** 31 < x < 2 ** 31 - 1:
+            return x
+        return 0
