dd929abef7ff246c9b639409558cac3ebaac8dca
litao <593516104@qq.com>
2021-03-21 22:05:57 +0800

    update 二叉树

--- 完成/字符串/反转字符串.py
+++ 完成/字符串/反转字符串.py
@@ -10,7 +10,7 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
         left = 0
-        right = len(s) - 1
+        right = len(s) - 1 
         while left < right:
             s[left],s[right] = s[right],s[left]
             left += 1
